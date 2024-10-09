ce131ec1a7f977348b49899c81521e5598417739
Raymond King Setia <ge87qas@mytum.de>
2024-10-09 20:59:01 +0200

    clean up ContentView

--- trip-tracker/ContentView.swift
+++ trip-tracker/ContentView.swift
@@ -14,25 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Spacer()
-            Button(action: addTrip) {
-                Image(systemName: "plus")
-                    .font(.largeTitle)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(20)
-            }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .trailing)
-            .padding(.trailing, 10)
         }
-    }
-
-    // Define the addTrip function
-    func addTrip() {
-        // Your code to add a trip goes here
-        print("Trip added!")
     }
 }
 

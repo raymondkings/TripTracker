500d32e646a530a83114036b1234683d317a7ceb
Raymond King Setia <ge87qas@mytum.de>
2024-10-09 23:53:21 +0200

    [A-1] : TripListView, no styling

---
+++ trip-tracker/Trip Subsystem/Trip/TripListView.swift
@@ -0,0 +1,39 @@
+//
+//  TripListView.swift
+//  trip-tracker
+//
+//  Created by Raymond King on 09.10.24.
+//
+import SwiftUI
+
+struct TripListView: View {
+    @ObservedObject var viewModel = TripViewModel()
+    @State private var isShowingCreateTrip = false
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 16) {
+                    ForEach(viewModel.trips) { trip in
+                        NavigationLink(destination: TripDetailView(trip: trip)) {
+                            TripCardView(trip: trip)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Trips")
+            .navigationBarItems(trailing: Button(action: {
+                isShowingCreateTrip.toggle()
+            }) {
+                Text("Add Trip")
+            })
+            .sheet(isPresented: $isShowingCreateTrip) {
+                CreateTrip(viewModel: viewModel)
+            }
+        }
+    }
+}
+
+#Preview {
+    TripListView()
+}

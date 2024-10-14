862517ba6a8f646aa38efb524267ea49ad399caf
Raymond King Setia <ge87qas@mytum.de>
2024-10-14 23:17:13 +0200

    resolve merge conflicts

--- trip-tracker/Trip Subsystem/Trip/TripListView.swift
+++ trip-tracker/Trip Subsystem/Trip/TripListView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct TripListView: View {
     @State var viewModel = TripViewModel()
     @State private var isShowingCreateTrip = false
-    @Environment(\.colorScheme) var colorScheme
-    @State private var isDarkMode = false
     @State private var imageViewModel = ImageViewModel()
     @State private var showSuccessToast = false
-
+    @State private var isDarkMode = false
+    
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -33,10 +34,11 @@ struct TripListView: View {
                 AlertToast(type: .complete(Color.green), title: "Trip Saved!")
             }
         }
+        .preferredColorScheme(isDarkMode ? .dark : .light)
     }
-
+    
     // MARK: - Extracted Methods
-
+    
     private func tripListContent() -> some View {
         VStack(spacing: 16) {
             ForEach(viewModel.trips) { trip in
@@ -55,22 +57,23 @@ struct TripListView: View {
         }
         .padding(16)
     }
-
+    
     private func addButton() -> some View {
         HStack {
-                    Button(action: {
-                        isDarkMode.toggle()
-                        toggleColorScheme()
-                    }) {
-                        Image(systemName: isDarkMode ? "moon.fill" : "sun.max.fill")
-                            .foregroundColor(isDarkMode ? .yellow : .blue)
-                    }
-                    Button(action: {
-                        isShowingCreateTrip.toggle()
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                }
+            Button(action: {
+                isDarkMode.toggle()
+                toggleColorScheme()
+            }) {
+                Image(systemName: isDarkMode ? "moon.fill" : "sun.max.fill")
+                    .foregroundColor(isDarkMode ? .yellow : .blue)
+            }
+            Button(action: {
+                isShowingCreateTrip.toggle()
+            }) {
+                Image(systemName: "plus")
+                    .frame(width: 44, height: 44)
+            }
+        }
     }
 
     private func toggleColorScheme() {
@@ -81,7 +84,7 @@ struct TripListView: View {
         }
     }
 }
-
+    
 #Preview {
     TripListView()
 }

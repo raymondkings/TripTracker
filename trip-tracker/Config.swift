0072c856d8dbacb1e10fc5ffe12b03641983538b
Raymond King Setia <ge87qas@mytum.de>
2024-10-26 14:45:23 +0200

    hide access key

--- trip-tracker/Config.swift
+++ trip-tracker/Config.swift
@@ -5,6 +5,6 @@
 //  Created by Raymond King on 26.10.24.
 //
 
-struct ConfigTemplate {
+struct Config {
     static let accessKey = "MnVxMNJF2r7WBgA7eDhJKkTFXe--PLpXh6lGUSDLgs0"
 }
---
+++ trip-tracker/ConfigTemplate.swift
@@ -0,0 +1,10 @@
+//
+//  Config.template.swift
+//  trip-tracker
+//
+//  Created by Raymond King on 26.10.24.
+//
+
+struct ConfigTemplate {
+    static let accessKey = "YOUR_ACCESS_KEY_HERE"
+}
